fe6d3bd1745cef6b69d5a499aea2bac2fbaef7d1
Ymurakami0910 <loey1._.127yun@icloud.com>
2025-04-15 17:11:14 -0700

    chips

--- src/components/ProjectOverview.jsx
+++ src/components/ProjectOverview.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 import styles from "./ProjectCard2.module.css";
 
+import aiIcon from "../assets/icons/ai.png";
+import psIcon from "../assets/icons/ps.png";
+import idIcon from "../assets/icons/id.png";
+import aeIcon from "../assets/icons/ae.png";
+import figmaIcon from "../assets/icons/figma.png";
+import htmlIcon from "../assets/icons/html.png";
+import cssIcon from "../assets/icons/css.png";
+import jsIcon from "../assets/icons/js.png";
+
+const chipIcons = {
+  Indesign: idIcon,
+  Illustrator: aiIcon,
+  PhotoShop: psIcon,
+  "After Effect": aeIcon,
+  Figma: figmaIcon,
+  HTML: htmlIcon,
+  CSS: cssIcon,
+  JS: jsIcon,
+  "Group Project": null, // No icon for this
+  "UI/UX": null, // No icon for this
+};
+
+
+
 const ProjectOverview = ({ title, description, chips }) => {
+  
   return (
     <section className="Project-overview Paper_v2 bg_pattern">
       <div className="container">
@@ -9,12 +34,24 @@ const ProjectOverview = ({ title, description, chips }) => {
           <h2>{title}</h2>
           <p>{description}</p>
           <div className={styles.chipsContainer}>
-            {chips?.map((chip, index) => (
-              <span className={styles.chip} key={index}>
-                {chip}
-              </span>
-            ))}
-          </div>
+  {Array.isArray(chips) && chips.map((chip, index) => (
+    <span key={index} className={styles.chip}>
+      {chipIcons[chip] ? (
+        <>
+          <img
+            src={chipIcons[chip]}
+            alt={chip}
+            className={styles.chipIcon}
+          />
+          <span className={styles.chipText}>{chip}</span>
+        </>
+      ) : (
+        <span className={styles.chipText}>{chip}</span>
+      )}
+    </span>
+  ))}
+</div>
+
         </div>
       </div>
     </section>

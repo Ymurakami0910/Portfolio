59e90a91cf2f1de48f1a99717add0d16c9e8af58
Ymurakami0910 <loey1._.127yun@icloud.com>
2026-01-17 14:32:42 -0800

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "",
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["react", "react-dom", "other-dependencies"],
+  },
+  build: {
+    emptyOutDir: true,
+    assetsDir: ".",
+  },
+});

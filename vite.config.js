5c2b357bfae4eac07234f12493fe6e9189136545
Ymurakami0910 <loey1._.127yun@icloud.com>
2026-01-17 14:39:12 -0800

    updated vite and deploy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   base: "",
   plugins: [react()],
-  optimizeDeps: {
-    include: ["react", "react-dom", "other-dependencies"],
-  },
   build: {
     emptyOutDir: true,
     assetsDir: ".",
